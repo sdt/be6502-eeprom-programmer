a82294e1f12ad2acec8540fb57355c41fda05bdf
Stephen Thirlwall <sdt@dr.com>
2019-10-06 12:47:50 +1100

    Fill a random buffer. Randomised each time.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <Arduino.h>
 #include "eeprom.h"
+#include "randomiser.h"
+
+#define ARRAY_COUNT(a)  (sizeof(a)/sizeof((a)[0]))
+
+static void fillBuffer(uint8_t *buf, int bytes);
+
+static uint8_t databuffer[1024];
 
 void setup() {
     Serial.begin(115200);
+    randomiser_init(Serial);
+    fillBuffer(databuffer, ARRAY_COUNT(databuffer));
     eeprom_init();
 }
 
 void loop() {
+}
+
+void xloop() {
     static char buf[256];
     static uint16_t address = 0;
     static uint8_t  data = 0x10;
@@ -38,4 +50,17 @@ void loop() {
 
     address = (address + 1) & 0x7fff;
     data++;
+}
+
+static void fillBuffer(uint8_t* buf, int bytes) {
+    // Computes the srecord checksum
+    uint8_t checksum = 0;
+    for ( ; bytes != 0; bytes--) {
+        uint8_t data = random(256);
+        checksum += data;
+        *buf++ = data;
+    }
+    checksum = ~checksum;
+    Serial.print("Buffer checksum: ");
+    Serial.println(checksum, DEC);
 }
