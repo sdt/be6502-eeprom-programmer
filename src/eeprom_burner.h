d1124ce6eb24d128dea9c02148d720269b5e5151
Stephen Thirlwall <sdt@dr.com>
2021-08-14 12:28:33 +1000

    Curse include guard's blocked nose
    o_O

--- src/eeprom_burner.h
+++ src/eeprom_burner.h
@@ -1,5 +1,5 @@
-#ifndef INCLUDE_EEPROM_BURDER_H
-#define INCLUDE_EEPROM_BURDER_H
+#ifndef INCLUDE_EEPROM_BURNER_H
+#define INCLUDE_EEPROM_BURNER_H
 
 #include <Arduino.h>
 
@@ -10,4 +10,4 @@ extern bool eb_writePage(uint16_t address, const uint8_t* data, uint8_t size);
 extern bool eb_verifyPage(uint16_t address, const uint8_t* data, uint8_t size);
 extern void eb_pinTest(HardwareSerial& serial);
 
-#endif // INCLUDE_EEPROM_BURDER_H
+#endif // INCLUDE_EEPROM_BURNER_H
