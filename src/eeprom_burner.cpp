0bd20c2d31b99f635cc62f25a15bbc2c206014be
Stephen Thirlwall <sdt@dr.com>
2021-08-14 18:24:09 +1000

    Implement most of Nano version
    Still need to work out the CE handling

--- src/eeprom_burner.cpp
+++ src/eeprom_burner.cpp
@@ -72,19 +72,126 @@
 
 #elif defined(ARDUINO_AVR_NANO)
 
-    static void initPins() { }
-    static void setAddress(uint16_t address) { }
-    static uint8_t readData() { return 0; }
-    static void writeData(uint8_t data) { }
-    static void setDataReadMode() { }
-    static void setDataWriteMode() { }
+    #include <avr/io.h>
+
+    // Data pins
+    // PB0-1:   D0-D1
+    // PD2-7:   D2-D7
+
+    // Address pins on SR1/SR2
+    //
+    // Shift register control
+    // PB2:     SS (must be output)
+    // PB3:     MOSI -> SER1 & SER2
+    // PB4:     MISO (input, but unconnected)
+    // PB5:     SCK -> SRCLK1 & SRCLK2
+    //
+    // PC2: OEB1 & OEB2
+    // PC3: RCLK1
+    // PC4: RCLK2
+    //
+    // PC0: ~WE
+    // PC1: ~OE
+    // ~CS is on A15
+
+    #define WRITE_MASKED(reg, value, mask) \
+        (reg) = ((reg) & ~(mask)) | (value)
+                                            // Initial DDr
+    const uint8_t PB0_Data0     = BIT(0);   // input
+    const uint8_t PB1_Data1     = BIT(1);   // input
+    const uint8_t PB2_SS        = BIT(2);   // output
+    const uint8_t PB3_MOSI      = BIT(3);   // output
+    const uint8_t PB4_MISO      = BIT(4);   // input-pullup
+    const uint8_t PB5_SCK       = BIT(5);   // output
+    const uint8_t PB_DataMask   = PB0_Data0 | PB1_Data1;
+    const uint8_t PB_MASK       = PB_DataMask | PB2_SS | PB3_MOSI
+                                | PB4_MISO | PB5_SCK;
+
+    const uint8_t PC0_ROM_WEB   = BIT(0);   // output
+    const uint8_t PC1_ROM_OEB   = BIT(1);   // output
+    const uint8_t PC2_SR_OEB    = BIT(2);   // output
+    const uint8_t PC3_RCLK1     = BIT(3);   // output
+    const uint8_t PC4_RCLK2     = BIT(4);   // output
+    const uint8_t PC_MASK       = PC0_ROM_WEB | PC1_ROM_OEB | PC2_SR_OEB
+                                | PC3_RCLK1 | PC4_RCLK2;
+
+    const uint8_t PD2_Data2     = BIT(2);   // input
+    const uint8_t PD3_Data3     = BIT(3);   // input
+    const uint8_t PD4_Data4     = BIT(4);   // input
+    const uint8_t PD5_Data5     = BIT(5);   // input
+    const uint8_t PD6_Data6     = BIT(6);   // input
+    const uint8_t PD7_Data7     = BIT(7);   // input
+    const uint8_t PD_DataMask   = PD2_Data2 | PD3_Data3 | PD4_Data4
+                                | PD5_Data5 | PD6_Data6 | PD7_Data7;
+    const uint8_t PD_MASK       = PD_DataMask;
+
+    static void setAddress(uint16_t address);
+    static void initPins() {
+        WRITE_MASKED(PORT_DIR(B), PB2_SS | PB3_MOSI | PB5_SCK, PB_MASK);
+
+        // Set MISO to input-pullup. Clear SCK.
+        WRITE_MASKED(PORT_OUT(B), PB4_MISO, PB4_MISO | PB5_SCK);
+
+        WRITE_MASKED(PORT_DIR(C), PC_MASK, PC_MASK);
+
+        // Set PC0_ROM_WEB & PC1_ROM_OEB high. Other pins low.
+        WRITE_MASKED(PORT_OUT(C), PC0_ROM_WEB | PC1_ROM_OEB, PC_MASK);
+
+        WRITE_MASKED(PORT_DIR(D), 0, PD_MASK);
+
+        // SPI mode zero, master, MSB first.
+        SPCR = BIT(SPE) | BIT(MSTR) | BIT(SPR1) | BIT(SPR0);
+    }
+
+    static uint8_t spiSend(uint8_t data) {
+        SPDR = data;
+        while ((SPSR & BIT(SPIF)) == 0) {
+            // wait for SPIF
+        }
+        return data;
+    }
+
+    static void setAddress(uint16_t address) {
+        uint8_t addr_low  = address & 0x00ff;
+        uint8_t addr_high = address >> 8;
+
+        //Serial.print("Address: "); Serial.println(address, HEX);
+
+        // High byte first
+        spiSend(addr_high);
+        spiSend(addr_low);
+
+        // Strobe the RCLK pins.
+        WRITE_MASKED(PORT_OUT(C), PC3_RCLK1 | PC4_RCLK2, PC3_RCLK1 | PC4_RCLK2);
+        WRITE_MASKED(PORT_OUT(C), 0, PC3_RCLK1 | PC4_RCLK2);
+    }
+
+    static uint8_t readData() {
+        return (PORT_IN(B) & PB_DataMask)
+             | (PORT_IN(D) & PD_DataMask);
+    }
+
+    static void writeData(uint8_t data) {
+        WRITE_MASKED(PORT_OUT(B), data & PB_DataMask, PB_DataMask);
+        WRITE_MASKED(PORT_OUT(D), data & PD_DataMask, PD_DataMask);
+    }
+
+    static void setDataReadMode() {
+        WRITE_MASKED(PORT_DIR(B), 0, PB_DataMask);
+        WRITE_MASKED(PORT_DIR(D), 0, PD_DataMask);
+    }
+
+    static void setDataWriteMode() {
+        WRITE_MASKED(PORT_DIR(B), PB_DataMask, PB_DataMask);
+        WRITE_MASKED(PORT_DIR(D), PD_DataMask, PD_DataMask);
+    }
 
     static void chipSelectOn()      { }
     static void chipSelectOff()     { }
 
     #define CONTROL C
-    const uint8_t Control_OE = BIT(4);
-    const uint8_t Control_WE = BIT(5);
+    const uint8_t Control_OE = BIT(1);
+    const uint8_t Control_WE = BIT(0);
 #else
     #error "Building for UNKNOWN"
 #endif
@@ -228,22 +335,22 @@ static void waitForKey(HardwareSerial& serial) {
     }
 }
 
+#define SKIP_PIN(number, func) \
+    serial.println("Pin " #number " is " func); \
+    waitForKey(serial);
+
+#define SET_PIN(number, port, bit)  \
+    PORT_DIR(port) |= 1<<((bit)&0x7); \
+    PORT_OUT(port) |= 1<<((bit)&0x7); \
+    serial.println("Pin " #number); \
+    waitForKey(serial); \
+    PORT_OUT(port) &= ~(1<<((bit)&0x7));
+
 #if defined(ARDUINO_AVR_MEGA2560)
 
-    #define SET_PIN(number, port, bit)  \
-        PORT_DIR(port) |= 1<<(bit); \
-        PORT_OUT(port) |= 1<<(bit); \
-        serial.println("Pin " #number); \
-        waitForKey(serial); \
-        PORT_OUT(port) &= ~(1<<(bit));
-
-    #define SKIP_PIN(number, func) \
-        serial.println("Pin " #number " is " func); \
-        waitForKey(serial);
-
     void eb_pinTest(HardwareSerial& serial) {
-        SET_PIN(1,   ADDR_HIGH, 14-8);
-        SET_PIN(2,   ADDR_HIGH, 12-8);
+        SET_PIN(1,   ADDR_HIGH, 14);
+        SET_PIN(2,   ADDR_HIGH, 12);
         SET_PIN(3,   ADDR_LOW,  7);
         SET_PIN(4,   ADDR_LOW,  6);
         SET_PIN(5,   ADDR_LOW,  5);
@@ -263,19 +370,60 @@ static void waitForKey(HardwareSerial& serial) {
         SET_PIN(18,  DATA,      6);
         SET_PIN(19,  DATA,      7);
         SET_PIN(20,  CONTROL,   4);
-        SET_PIN(21,  ADDR_HIGH, 10-8);
+        SET_PIN(21,  ADDR_HIGH, 10);
         SET_PIN(22,  CONTROL,   5);
-        SET_PIN(23,  ADDR_HIGH, 11-8);
-        SET_PIN(24,  ADDR_HIGH, 9-8);
-        SET_PIN(25,  ADDR_HIGH, 8-8);
-        SET_PIN(26,  ADDR_HIGH, 13-8);
+        SET_PIN(23,  ADDR_HIGH, 11);
+        SET_PIN(24,  ADDR_HIGH, 9);
+        SET_PIN(25,  ADDR_HIGH, 8);
+        SET_PIN(26,  ADDR_HIGH, 13);
         SET_PIN(27,  CONTROL,   6);
         SKIP_PIN(28,  "+5V");
     }
 
 #elif defined(ARDUINO_AVR_NANO)
+
+    #define SET_ADDR_PIN(number, addr_bit) \
+        setAddress(1 << (addr_bit)); \
+        serial.println("Pin " #number); \
+        waitForKey(serial); \
+        setAddress(0);
+
+    #define SET_DATA_PIN(number, data_bit) \
+        setDataWriteMode(); \
+        writeData(1 << (data_bit)); \
+        serial.println("Pin " #number); \
+        waitForKey(serial); \
+        writeData(0);
+
     void eb_pinTest(HardwareSerial& serial) {
-        serial.println("Woo!");
-        waitForKey(serial);
+        SET_ADDR_PIN(1, 14);
+        SET_ADDR_PIN(2, 12);
+        SET_ADDR_PIN(3,  7);
+        SET_ADDR_PIN(4,  6);
+        SET_ADDR_PIN(5,  5);
+        SET_ADDR_PIN(6,  4);
+        SET_ADDR_PIN(7,  3);
+        SET_ADDR_PIN(8,  2);
+        SET_ADDR_PIN(9,  1);
+        SET_ADDR_PIN(10, 0);
+        SET_DATA_PIN(11, 0);
+        SET_DATA_PIN(12, 1);
+        SET_DATA_PIN(13, 2);
+        SKIP_PIN(14, "ground");
+
+        SET_DATA_PIN(15, 3);
+        SET_DATA_PIN(16, 4);
+        SET_DATA_PIN(17, 5);
+        SET_DATA_PIN(18, 6);
+        SET_DATA_PIN(19, 7);
+        SET_ADDR_PIN(20, 15);
+        SET_ADDR_PIN(21, 10);
+        SET_PIN(22, CONTROL, 1);
+        SET_ADDR_PIN(23, 11);
+        SET_ADDR_PIN(24,  9);
+        SET_ADDR_PIN(25,  8);
+        SET_ADDR_PIN(26, 13);
+        SET_PIN(27, CONTROL, 0);
+        SKIP_PIN(28,  "+5V");
     }
 #endif
